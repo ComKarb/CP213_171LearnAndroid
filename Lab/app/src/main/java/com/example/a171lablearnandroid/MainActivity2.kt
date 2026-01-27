ad85cf461286d3fdeb75289f192b9ca73c04c8cf
ComKarb <comandpeat@gmail.com>
2026-01-27 16:24:36 +0700

    add hp card screen

--- Lab/app/src/main/java/com/example/a171lablearnandroid/MainActivity2.kt
+++ Lab/app/src/main/java/com/example/a171lablearnandroid/MainActivity2.kt
@@ -11,7 +11,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.a171lablearnandroid.ui.theme._171LabLearnAndroidTheme
@@ -35,14 +40,20 @@ class MainActivity2 : ComponentActivity() {
 
 @Composable
 fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+    var inputText by remember { mutableStateOf(value="") }
+    Column{
+        Text(
+        text = "Hello $name! say = "+inputText,
         modifier = modifier
     )
-    Text(
-        text = "good bye $name!",
-        modifier = modifier
-    )
+        TextField(
+            value = inputText,
+            onValueChange = {
+                inputText = it
+            }
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
