41bb5fa821a27b141703d1bf16263a67e9447176
phumipat meeros <comandpeat@gmail.com>
2026-04-07 16:38:52 +0700

    Create Part10

--- Lab/app/src/main/java/com/example/a171lablearnandroid/MenuActivity.kt
+++ Lab/app/src/main/java/com/example/a171lablearnandroid/MenuActivity.kt
@@ -186,6 +186,15 @@ class MenuActivity : ComponentActivity() {
                             Text(text = "Part10Activity (Glance App Widget)")
                         }
                     }
+
+                    item {
+                        Button(onClick = {
+                            val options = ActivityOptionsCompat.makeBasic()
+                            startActivity(Intent(this@MenuActivity, Part11Activity::class.java), options.toBundle())
+                        }) {
+                            Text(text = "Part11Activity (Skeleton Loading)")
+                        }
+                    }
                 }
             }
         }
